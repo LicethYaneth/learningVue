89809c0c0b871d42affc5ec68021e6218721d9b0
Paula Alvarez Quintero <paulalzq@pop-os.localdomain>
2021-01-16 15:26:39 -0500

    vue-cli

---
+++ mindground-shop/mindground/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="MindGround logo" src="./assets/logo-blanco.svg">
+  <Titulo texto="MINDGROUND"/>
+  <Cuenta />
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Titulo from './components/Titulo.vue'
+import Cuenta from './components/Cuenta.vue'
+export default {
+  name: 'App',
+  components: {
+  Cuenta,
+  Titulo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ mindground-shop/mindground/src/components/Cuenta.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Resumen de tu orden</h1>
+  <h3>Monto final a pagar: {{total}}<br>
+  Medio de pago: {{pago}}<br>
+  Pago Contra Entrega: {{contra_e ? 'Disponible' : 'No disponible'}}</h3>
+  <div v-for="(servicio, index) in productos" :key="index">
+      {{index+1}}. {{ servicio }}
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            total: '$294.800',
+            pago: 'Tarjeta de crédito',
+            contra_e: true,
+            productos: ['Combo Escritorio + Biblioteca','Centro de cómputo maderkit color wengue']
+
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
