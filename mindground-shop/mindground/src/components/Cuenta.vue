c3899f451f91eeb2b4c2b2fb26790d2358037ece
Liceth Yaneth Rozo Quintana <liceth@pop-os.localdomain>
2021-01-21 14:19:19 -0500

    calendario

--- mindground-shop/mindground/src/components/Cuenta.vue
+++ mindground-shop/mindground/src/components/Cuenta.vue
@@ -6,19 +6,33 @@
   <div v-for="(servicio, index) in productos" :key="index">
       {{index+1}}. {{ servicio }}
   </div>
+  <Total />
 </template>
 
 <script>
+import Total from './Total'
 export default {
+    components: {
+        Total
+    },
     data() {
         return {
             total: '$294.800',
             pago: 'Tarjeta de crédito',
             contra_e: true,
+            envio_p:0,
             productos: ['Combo Escritorio + Biblioteca','Centro de cómputo maderkit color wengue']
 
         }
     },
+    methods: {
+        envio(){
+            {{this.contra_e ? this.envio_p=7500 : this.envio_p=0}}
+        },
+        total_c(){
+            return this.total+this.envio_p
+        }
+    }
 
 }
 </script>
---
+++ mindground-shop/mindground/src/components/Total.vue
@@ -0,0 +1,14 @@
+<template>
+<button>Calcular envio </button>
+<button>Calcular total </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
